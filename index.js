eb8a4523314a54a7c43c551d7bf41c06c441459e
Sheryll Dumapal <sheryllhhs@outlook.com>
2025-02-22 19:53:02 +0100

    added first routing

--- index.js
+++ index.js
@@ -10,7 +10,17 @@ const app = express();
 // Routing
 
 app.get("/", (req, res) => {
-  res.json("Welcome to the investment API");
+  res.json("Welcome to the Investment API");
 });
+
+app.get("/investor", (res, req) => {
+
+    axios.get("")
+        .then((response) => {
+            const html = response.data
+            console.log(html)
+        })
+
+})
 
 app.listen(PORT, () => console.log(`Server running on PORT ${{ PORT }}`));
