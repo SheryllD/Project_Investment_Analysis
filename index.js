5e2ffd4b597436f8b8bef49f9d9bacf264f142c5
Sheryll Dumapal <sheryllhhs@outlook.com>
2025-02-22 19:01:24 +0100

    PORT is listening, everything is working

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-// This is the file where I create the API 
+// This is the file where I create the API
 
-const PORT = 8000
-const express = require('express')
-const axios = require('axios')
-const cheerio = require('cheerio')
+const PORT = 8000;
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
 
 const app = express();
 
-app.listen(PORT, () => console.log(`Server running on PORT ${{ PORT }}`))
+// Routing
+
+app.get("/", (req, res) => {
+  res.json("Welcome to the investment API");
+});
+
+app.listen(PORT, () => console.log(`Server running on PORT ${{ PORT }}`));
