e7585be87a49a37080127192e72e886f4433b8b2
Sheryll Dumapal <sheryllhhs@outlook.com>
2025-02-22 15:24:31 +0100

    nothing fancy

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // This is the file where I create the API 
+
+const PORT = 8000
+const express = require('express')
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+const app = express();
+
+app.listen(PORT, () => console.log(`Server running on PORT ${{ PORT }}`))
